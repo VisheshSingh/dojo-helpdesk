c60f0e594fd33eced5300230b768981d5b14809b
Vishesh Singh <visheshsingh16@gmail.com>
2026-02-17 22:40:52 -0500

    revalidation

--- app/components/TicketList.jsx
+++ app/components/TicketList.jsx
@@ -1,5 +1,7 @@
 async function getTickets() {
-  const res = await fetch('http://localhost:4000/tickets');
+  const res = await fetch('http://localhost:4000/tickets', {
+    next: { revalidate: 30 },
+  });
   return res.json();
 }
 
