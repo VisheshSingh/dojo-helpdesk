12d1f8e20f6bc92338a0ac8fcbcf9d740518674d
Vishesh Singh <visheshsingh16@gmail.com>
2026-02-22 14:15:54 -0500

    Suspense boundary

--- app/tickets/page.jsx
+++ app/tickets/page.jsx
@@ -1,12 +1,18 @@
+import { Suspense } from 'react';
 import TicketList from '../components/TicketList';
+import Loading from '../loading';
 
 export default async function Tickets() {
   return (
     <main>
       <h1>Tickets</h1>
-      <p><small>Curently open tickets</small></p>
+      <p>
+        <small>Curently open tickets</small>
+      </p>
 
-      <TicketList />
+      <Suspense fallback={<Loading />}>
+        <TicketList />
+      </Suspense>
     </main>
   );
 }
